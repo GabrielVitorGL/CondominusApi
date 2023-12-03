aff3e236aa83053f6c53db499b0a4bb430df22b9
GabrielVitorGL <gabrielvitorcom@hotmail.com>
2023-12-03 14:52:59 -0300

    adicionando put e delete em area comum

--- Controllers/AreasComunsController.cs
+++ Controllers/AreasComunsController.cs
@@ -14,19 +14,19 @@ namespace CondominusApi.Controllers
     [Route("[controller]")]
     public class AreasComunsController : ControllerBase
     {
-        private readonly DataContext _context; 
+        private readonly DataContext _context;
 
         public AreasComunsController(DataContext context)
         {
             _context = context;
-        } 
+        }
 
         [HttpGet("GetAll")]
         public async Task<IActionResult> ListarAsync()
         {
             try
             {
-                List<AreaComum> areasComuns = await _context.AreasComuns.ToListAsync();                
+                List<AreaComum> areasComuns = await _context.AreasComuns.ToListAsync();
                 return Ok(areasComuns);
             }
             catch (System.Exception ex)
@@ -50,5 +50,64 @@ namespace CondominusApi.Controllers
                 return BadRequest(ex.Message);
             }
         }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(AreaComum areaComum)
+        {
+            try
+            {
+                AreaComum ac = await _context.AreasComuns
+                    .FirstOrDefaultAsync(x => x.Id == areaComum.Id);
+
+                if (ac == null)
+                    return NotFound("Área comum não encontrada");
+
+                ac.Nome = areaComum.Nome;
+
+                _context.AreasComuns.Update(ac);
+                await _context.SaveChangesAsync();
+
+                return Ok(ac.Id);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("DeletarMuitos")]
+        public async Task<IActionResult> DeleteAreasComuns([FromBody] int[] ids)
+        {
+            try
+            {
+                if (ids == null || ids.Length == 0)
+                {
+                    throw new Exception("Selecione áreas comuns para deletar.");
+                }
+
+                // Filtrar apenas IDs válidos e existentes no banco de dados
+                var areascomunsParaDeletar = await _context.AreasComuns
+                    .Where(u => ids.Contains(u.Id))
+                    .ToListAsync();
+
+                if (areascomunsParaDeletar.Count == 0)
+                {
+                    return NotFound("Nenhuma área comum encontrada para os IDs fornecidos.");
+                }
+
+                _context.AreasComuns.RemoveRange(areascomunsParaDeletar);
+
+                int linhasAfetadas = await _context.SaveChangesAsync();
+
+                // Após deletar as áreas comuns, recupere a lista atualizada
+                var listaAtualizada = await _context.AreasComuns.ToListAsync();
+                return Ok(listaAtualizada);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
     }
 }
