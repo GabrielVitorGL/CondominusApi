7a0f89589f9015cdeeaacb7b74bc0c58fba3e136
GabrielVitorGL <gabrielvitorcom@hotmail.com>
2023-12-04 12:33:05 -0300

    adicionando put e delete em apartamento e dependente

--- Controllers/ApartamentosController.cs
+++ Controllers/ApartamentosController.cs
@@ -14,7 +14,7 @@ namespace CondominusApi.Controllers
     [Route("[controller]")]
     public class ApartamentosController : ControllerBase
     {
-        private readonly DataContext _context; 
+        private readonly DataContext _context;
 
         public ApartamentosController(DataContext context)
         {
@@ -26,7 +26,7 @@ namespace CondominusApi.Controllers
         {
             try
             {
-                List<Apartamento> apartamentos = await _context.Apartamentos.ToListAsync();                
+                List<Apartamento> apartamentos = await _context.Apartamentos.ToListAsync();
                 return Ok(apartamentos);
             }
             catch (System.Exception ex)
@@ -46,6 +46,68 @@ namespace CondominusApi.Controllers
                 return Ok(novoApartamento.Id);
             }
             catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(Apartamento apartamento)
+        {
+            try
+            {
+                Apartamento ap = await _context.Apartamentos
+                    .FirstOrDefaultAsync(x => x.Id == apartamento.Id);
+
+                if (ap != null)
+                {
+                    ap.Numero = apartamento.Numero;
+                    ap.Telefone = apartamento.Telefone;
+
+                    _context.Apartamentos.Update(ap);
+                    await _context.SaveChangesAsync();
+                    return Ok(ap.Id);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("DeletarMuitos")]
+        public async Task<IActionResult> DeleteApartamentos([FromBody] int[] ids)
+        {
+            try
+            {
+                if (ids == null || ids.Length == 0)
+                {
+                    throw new Exception("Selecione apartamentos para deletar.");
+                }
+
+                // Filtrar apenas IDs válidos e existentes no banco de dados
+                var apartamentosParaDeletar = await _context.Apartamentos
+                    .Where(u => ids.Contains(u.Id))
+                    .ToListAsync();
+
+                if (apartamentosParaDeletar.Count == 0)
+                {
+                    return NotFound("Nenhum apartamento encontrado para os IDs fornecidos.");
+                }
+
+                _context.Apartamentos.RemoveRange(apartamentosParaDeletar);
+
+                int linhasAfetadas = await _context.SaveChangesAsync();
+
+                // Após deletar as áreas comuns, recupere a lista atualizada
+                var listaAtualizada = await _context.Apartamentos.ToListAsync();
+                return Ok(listaAtualizada);
+            }
+            catch (Exception ex)
             {
                 return BadRequest(ex.Message);
             }
--- Controllers/DependentesController.cs
+++ Controllers/DependentesController.cs
@@ -14,12 +14,12 @@ namespace CondominusApi.Controllers
     [Route("[controller]")]
     public class DependentesController : ControllerBase
     {
-        private readonly DataContext _context; 
+        private readonly DataContext _context;
 
         public DependentesController(DataContext context)
         {
             _context = context;
-        } 
+        }
 
         //listagem geral de pessoas
         [HttpGet("GetAll")]
@@ -51,6 +51,68 @@ namespace CondominusApi.Controllers
                 return Ok(novoDependente.Id);
             }
             catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(Dependente dependente)
+        {
+            try
+            {
+                Dependente dp = await _context.Dependentes
+                    .FirstOrDefaultAsync(x => x.Id == dependente.Id);
+
+                if (dp != null)
+                {
+                    dp.Nome = dependente.Nome;
+                    dp.Telefone = dependente.Telefone;
+
+                    _context.Dependentes.Update(dp);
+                    await _context.SaveChangesAsync();
+                    return Ok(dp.Id);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("DeletarMuitos")]
+        public async Task<IActionResult> DeleteDependentes([FromBody] int[] ids)
+        {
+            try
+            {
+                if (ids == null || ids.Length == 0)
+                {
+                    throw new Exception("Selecione dependentes para deletar.");
+                }
+
+                // Filtrar apenas IDs válidos e existentes no banco de dados
+                var dependentesParaDeletar = await _context.Dependentes
+                    .Where(u => ids.Contains(u.Id))
+                    .ToListAsync();
+
+                if (dependentesParaDeletar.Count == 0)
+                {
+                    return NotFound("Nenhum dependente encontrado para os IDs fornecidos.");
+                }
+
+                _context.Dependentes.RemoveRange(dependentesParaDeletar);
+
+                int linhasAfetadas = await _context.SaveChangesAsync();
+
+                // Após deletar as áreas comuns, recupere a lista atualizada
+                var listaAtualizada = await _context.Dependentes.ToListAsync();
+                return Ok(listaAtualizada);
+            }
+            catch (Exception ex)
             {
                 return BadRequest(ex.Message);
             }
